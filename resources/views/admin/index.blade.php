61597d5595919800f62abfc73460de7e23807b8b
raihanaselole <raiham.rp@gmail.com>
2024-12-09 16:35:24 +0700

    up template

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot name='page_name'>Halaman Dashboard</x-slot>
+    <x-slot name='page_content'>
+        Selamat Datang di Halaman Dashboard
+    </x-slot>
+</x-layout>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,66 +1,162 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-  <!-- Brand Logo -->
-  <a href="/dashboard" class="brand-link">
-    <img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">Seraphic Elegance</span>
+<aside class="sidebar">
+  <!-- sidebar close btn -->
+   <button type="button" class="sidebar-close-btn text-gray-500 hover-text-white hover-bg-main-600 text-md w-24 h-24 border border-gray-100 hover-border-main-600 d-xl-none d-flex flex-center rounded-circle position-absolute"><i class="ph ph-x"></i></button>
+  <!-- sidebar close btn -->
+  
+  <a href="index.html" class="sidebar__logo text-center p-20 position-sticky inset-block-start-0 bg-white w-100 z-1 pb-10">
+      <img src="assets/images/logo/logo.png" alt="Logo">
   </a>
 
-  <!-- Sidebar -->
-  <div class="sidebar">
-    <!-- Sidebar user (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a href="#" class="d-block"></a>
-        <span class="text-primary"></span>
-      </div>
-    </div>
+  <div class="sidebar-menu-wrapper overflow-y-auto scroll-sm">
+      <div class="p-20 pt-10">
+          <ul class="sidebar-menu">
+              <li class="sidebar-menu__item has-dropdown">
+                  <a href="javascript:void(0)" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-squares-four"></i></span>
+                      <span class="text">Dashboard</span>
+                      <span class="link-badge">3</span>
+                  </a>
+                  <!-- Submenu start -->
+                  <ul class="sidebar-submenu">
+                      <li class="sidebar-submenu__item">
+                          <a href="index.html" class="sidebar-submenu__link"> Dashboard One </a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="index-2.html" class="sidebar-submenu__link"> Dashboard Two </a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="index-3.html" class="sidebar-submenu__link"> Dashboard Three </a>
+                      </li>
+                  </ul>
+                  <!-- Submenu End -->
+              </li>
+              <li class="sidebar-menu__item has-dropdown">
+                  <a href="javascript:void(0)" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-graduation-cap"></i></span>
+                      <span class="text">Courses</span>
+                  </a>
+                  <!-- Submenu start -->
+                  <ul class="sidebar-submenu">
+                      <li class="sidebar-submenu__item">
+                          <a href="student-courses.html" class="sidebar-submenu__link"> Student Courses </a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="mentor-courses.html" class="sidebar-submenu__link"> Mentor Courses </a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="create-course.html" class="sidebar-submenu__link"> Create Course </a>
+                      </li>
+                  </ul>
+                  <!-- Submenu End -->
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="students.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-users-three"></i></span>
+                      <span class="text">Students</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="assignment.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-clipboard-text"></i></span>
+                      <span class="text">Assignments</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="mentors.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-users"></i></span>
+                      <span class="text">Mentors</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="resources.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-bookmarks"></i></span>
+                      <span class="text">Resources</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="message.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-chats-teardrop"></i></span>
+                      <span class="text">Messages</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="analytics.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-chart-bar"></i></span>
+                      <span class="text">Analytics</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="event.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-calendar-dots"></i></span>
+                      <span class="text">Events</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="library.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-books"></i></span>
+                      <span class="text">Library</span>
+                  </a>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="pricing-plan.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-coins"></i></span>
+                      <span class="text">Pricing</span>
+                  </a>
+              </li>
+              
+              <li class="sidebar-menu__item">
+                  <span class="text-gray-300 text-sm px-20 pt-20 fw-semibold border-top border-gray-100 d-block text-uppercase">Settings</span>
+              </li>
+              <li class="sidebar-menu__item">
+                  <a href="setting.html" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-gear"></i></span>
+                      <span class="text">Account Settings</span>
+                  </a>
+              </li>
 
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-      <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <!-- Sidebar Menu -->
-    <nav class="mt-2">
-      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
-            <p>
-              Dashboard
-              <i class="right fas fa-angle-left"></i>
-            </p>
-          </a>
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="/dashboard/produk" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Produk</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/dashboard/testimoni" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Testimoni</p>
-              </a>
-            </li>
+              <li class="sidebar-menu__item has-dropdown">
+                  <a href="javascript:void(0)" class="sidebar-menu__link">
+                      <span class="icon"><i class="ph ph-shield-check"></i></span>
+                      <span class="text">Authetication</span>
+                  </a>
+                  <!-- Submenu start -->
+                  <ul class="sidebar-submenu">
+                      <li class="sidebar-submenu__item">
+                          <a href="sign-in.html" class="sidebar-submenu__link">Sign In</a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="sign-up.html" class="sidebar-submenu__link">Sign Up</a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="forgot-password.html" class="sidebar-submenu__link">Forgot Password</a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="reset-password.html" class="sidebar-submenu__link">Reset Password</a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="verify-email.html" class="sidebar-submenu__link">Verify Email</a>
+                      </li>
+                      <li class="sidebar-submenu__item">
+                          <a href="two-step-verification.html" class="sidebar-submenu__link">Two Step Verification</a>
+                      </li>
+                  </ul>
+                  <!-- Submenu End -->
+              </li>
+              
           </ul>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.sidebar-menu -->
+      </div>
+      <div class="p-20 pt-80">
+          <div class="bg-main-50 p-20 pt-0 rounded-16 text-center mt-74">
+              <span class="border border-5 bg-white mx-auto border-primary-50 w-114 h-114 rounded-circle flex-center text-success-600 text-2xl translate-n74">
+                  <img src="assets/images/icons/certificate.png" alt="" class="centerised-img">
+              </span>
+              <div class="mt-n74">
+                  <h5 class="mb-4 mt-22">Get Pro Certificate</h5>
+                  <p class="">Explore 400+ courses with lifetime members</p>
+                  <a href="pricing-plan.html" class="btn btn-main mt-16 rounded-pill">Get Access</a>
+              </div>
+          </div>
+      </div>
   </div>
-  <!-- /.sidebar -->
-</aside>
+
+</aside> 
