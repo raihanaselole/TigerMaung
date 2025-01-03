293dc088356cb5212bf6359475d426a4a0e5366b
raihanaselole <raiham.rp@gmail.com>
2025-01-04 00:21:10 +0700

    finishing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -91,7 +91,7 @@
                         <div class="card-body">
                             <div class="mb-20 flex-between flex-wrap gap-8">
                                 <h4 class="mb-0">Top Courses Pick for You</h4>
-                                <a href="student-courses.html" class="text-13 fw-medium text-main-600 hover-text-decoration-underline">See All</a>
+                                <a href="#" class="text-13 fw-medium text-main-600 hover-text-decoration-underline">See All</a>
                             </div>
                             
                             <div class="row g-20">
@@ -99,16 +99,16 @@
                                     <div class="card border border-gray-100">
                                         <div class="card-body p-8">
                                             <a href="course-details.html" class="bg-main-100 rounded-8 overflow-hidden text-center mb-8 h-164 flex-center p-8">
-                                                <img src="images/thumbs/course-img1.png" alt="Course Image">
+                                                <img src="{{ asset('admin/images/fs.png')}}" alt="Course Image">
                                             </a>
                                             <div class="p-8">
                                                 <span class="text-13 py-2 px-10 rounded-pill bg-success-50 text-success-600 mb-16">Development</span>
                                                 <h5 class="mb-0"><a href="course-details.html" class="hover-text-main-600">Full Stack Web Development</a></h5>
 
                                                 <div class="flex-align gap-8 flex-wrap mt-16">
-                                                    <img src="images/thumbs/user-img1.png" class="w-28 h-28 rounded-circle object-fit-cover" alt="User Image">
+                                                    <img src="{{ asset('admin/images/4.svg')}}" class="w-28 h-28 rounded-circle object-fit-cover" alt="User Image">
                                                     <div>
-                                                        <span class="text-gray-600 text-13">Created by <a href="profile.html" class="fw-semibold text-gray-700 hover-text-main-600 hover-text-decoration-underline">Albert James</a> </span>
+                                                        <span class="text-gray-600 text-13">Created by <a href="#" class="fw-semibold text-gray-700 hover-text-main-600 hover-text-decoration-underline">TUTU</a> </span>
                                                     </div>
                                                 </div>
 
@@ -129,7 +129,7 @@
                                                         <span class="text-13 fw-bold text-gray-600">4.9</span>
                                                         <span class="text-13 fw-bold text-gray-600">(12k)</span>
                                                     </div>
-                                                    <a href="course-details.html" class="btn btn-outline-main rounded-pill py-9">View Details</a>
+                                                    <a href="#" class="btn btn-outline-main rounded-pill py-9">View Details</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -139,16 +139,16 @@
                                     <div class="card border border-gray-100">
                                         <div class="card-body p-8">
                                             <a href="course-details.html" class="bg-main-100 rounded-8 overflow-hidden text-center mb-8 h-164 flex-center p-8">
-                                                <img src="images/thumbs/course-img5.png" alt="Course Image">
+                                                <img src="{{ asset('admin/images/design.png')}}" alt="Course Image">
                                             </a>
                                             <div class="p-8">
                                                 <span class="text-13 py-2 px-10 rounded-pill bg-warning-50 text-warning-600 mb-16">Design</span>
-                                                <h5 class="mb-0"><a href="course-details.html" class="hover-text-main-600">Design System</a></h5>
+                                                <h5 class="mb-0"><a href="#" class="hover-text-main-600">Design System</a></h5>
 
                                                 <div class="flex-align gap-8 flex-wrap mt-16">
-                                                    <img src="images/thumbs/user-img5.png" class="w-28 h-28 rounded-circle object-fit-cover" alt="User Image">
+                                                    <img src="{{ asset('admin/images/4.svg')}}" class="w-28 h-28 rounded-circle object-fit-cover" alt="User Image">
                                                     <div>
-                                                        <span class="text-gray-600 text-13">Created by <a href="profile.html" class="fw-semibold text-gray-700 hover-text-main-600 hover-text-decoration-underline">Albert James</a> </span>
+                                                        <span class="text-gray-600 text-13">Created by <a href="#" class="fw-semibold text-gray-700 hover-text-main-600 hover-text-decoration-underline">TUTU</a> </span>
                                                     </div>
                                                 </div>
 
@@ -169,7 +169,7 @@
                                                         <span class="text-13 fw-bold text-gray-600">4.9</span>
                                                         <span class="text-13 fw-bold text-gray-600">(12k)</span>
                                                     </div>
-                                                    <a href="course-details.html" class="btn btn-outline-main rounded-pill py-9">View Details</a>
+                                                    <a href="#" class="btn btn-outline-main rounded-pill py-9">View Details</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -179,16 +179,16 @@
                                     <div class="card border border-gray-100">
                                         <div class="card-body p-8">
                                             <a href="course-details.html" class="bg-main-100 rounded-8 overflow-hidden text-center mb-8 h-164 flex-center p-8">
-                                                <img src="images/thumbs/course-img6.png" alt="Course Image">
+                                                <img src="{{ asset('admin/images/react.png')}}" alt="Course Image">
                                             </a>
                                             <div class="p-8">
                                                 <span class="text-13 py-2 px-10 rounded-pill bg-danger-50 text-danger-600 mb-16">Frontend</span>
-                                                <h5 class="mb-0"><a href="course-details.html" class="hover-text-main-600">React Native Courese</a></h5>
+                                                <h5 class="mb-0"><a href="#" class="hover-text-main-600">React Native Courese</a></h5>
 
                                                 <div class="flex-align gap-8 flex-wrap mt-16">
-                                                    <img src="images/thumbs/user-img6.png" class="w-28 h-28 rounded-circle object-fit-cover" alt="User Image">
+                                                    <img src="{{ asset('admin/images/4.svg')}}" class="w-28 h-28 rounded-circle object-fit-cover" alt="User Image">
                                                     <div>
-                                                        <span class="text-gray-600 text-13">Created by <a href="profile.html" class="fw-semibold text-gray-700 hover-text-main-600 hover-text-decoration-underline">Albert James</a> </span>
+                                                        <span class="text-gray-600 text-13">Created by <a href="#" class="fw-semibold text-gray-700 hover-text-main-600 hover-text-decoration-underline">TUTU</a> </span>
                                                     </div>
                                                 </div>
 
@@ -209,7 +209,7 @@
                                                         <span class="text-13 fw-bold text-gray-600">4.9</span>
                                                         <span class="text-13 fw-bold text-gray-600">(12k)</span>
                                                     </div>
-                                                    <a href="course-details.html" class="btn btn-outline-main rounded-pill py-9">View Details</a>
+                                                    <a href="#" class="btn btn-outline-main rounded-pill py-9">View Details</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -251,38 +251,38 @@
                     <div class="card mt-24">
                         <div class="card-body">
                             <div class="mb-20 flex-between flex-wrap gap-8">
-                                <h4 class="mb-0">Assignments</h4>
-                                <a href="assignment.html" class="text-13 fw-medium text-main-600 hover-text-decoration-underline">See All</a>
+                                <h4 class="mb-0">Feature</h4>
+                                <a href="#" class="text-13 fw-medium text-main-600 hover-text-decoration-underline">See All</a>
                             </div>
                             <div class="p-xl-4 py-16 px-12 flex-between gap-8 rounded-8 border border-gray-100 hover-border-gray-200 transition-1 mb-16">
                                 <div class="flex-align flex-wrap gap-8">
                                     <span class="text-main-600 bg-main-50 w-44 h-44 rounded-circle flex-center text-2xl flex-shrink-0"><i class="ph-fill ph-graduation-cap"></i></span>
                                     <div>
-                                        <h6 class="mb-0">Do The Research</h6>
-                                        <span class="text-13 text-gray-400">Due in 9 days</span>
+                                        <h6 class="mb-0">Progress</h6>
+                                        <span class="text-13 text-gray-400">Melacak Progress</span>
                                     </div>
                                 </div>
-                                <a href="assignment.html" class="text-gray-900 hover-text-main-600"><i class="ph ph-caret-right"></i></a>
+                                <a href="#" class="text-gray-900 hover-text-main-600"><i class="ph ph-caret-right"></i></a>
                             </div>
                             <div class="p-xl-4 py-16 px-12 flex-between gap-8 rounded-8 border border-gray-100 hover-border-gray-200 transition-1 mb-16">
                                 <div class="flex-align flex-wrap gap-8">
-                                    <span class="text-main-600 bg-main-50 w-44 h-44 rounded-circle flex-center text-2xl flex-shrink-0"><i class="ph ph-code"></i></span>
+                                    <span class="text-main-600 bg-main-50 w-44 h-44 rounded-circle flex-center text-2xl flex-shrink-0"><i class="ph ph-users-three"></i></span>
                                     <div>
-                                        <h6 class="mb-0">PHP Dvelopment</h6>
-                                        <span class="text-13 text-gray-400">Due in 2 days</span>
+                                        <h6 class="mb-0">Reminder</h6>
+                                        <span class="text-13 text-gray-400">Pengingat Bimbingan</span>
                                     </div>
                                 </div>
-                                <a href="assignment.html" class="text-gray-900 hover-text-main-600"><i class="ph ph-caret-right"></i></a>
+                                <a href="#" class="text-gray-900 hover-text-main-600"><i class="ph ph-caret-right"></i></a>
                             </div>
                             <div class="p-xl-4 py-16 px-12 flex-between gap-8 rounded-8 border border-gray-100 hover-border-gray-200 transition-1">
                                 <div class="flex-align flex-wrap gap-8">
-                                    <span class="text-main-600 bg-main-50 w-44 h-44 rounded-circle flex-center text-2xl flex-shrink-0"><i class="ph ph-bezier-curve"></i></span>
+                                    <span class="text-main-600 bg-main-50 w-44 h-44 rounded-circle flex-center text-2xl flex-shrink-0"><i class="ph ph-chart-bar"></i></span>
                                     <div>
-                                        <h6 class="mb-0">Graphic Design</h6>
-                                        <span class="text-13 text-gray-400">Due in 5 days</span>
+                                        <h6 class="mb-0">Statistics</h6>
+                                        <span class="text-13 text-gray-400">Melihat Statistics</span>
                                     </div>
                                 </div>
-                                <a href="assignment.html" class="text-gray-900 hover-text-main-600"><i class="ph ph-caret-right"></i></a>
+                                <a href="#" class="text-gray-900 hover-text-main-600"><i class="ph ph-caret-right"></i></a>
                             </div>
                         </div>
                     </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -81,16 +81,6 @@
                 </ul>
             </div>
             <div class="p-20 pt-80">
-                <div class="bg-main-50 p-20 pt-0 rounded-16 text-center mt-74">
-                    <span class="border border-5 bg-white mx-auto border-primary-50 w-114 h-114 rounded-circle flex-center text-success-600 text-2xl translate-n74">
-                        <img src="assets/images/icons/certificate.png" alt="" class="centerised-img">
-                    </span>
-                    <div class="mt-n74">
-                        <h5 class="mb-4 mt-22">Get Pro Certificate</h5>
-                        <p class="">Explore 400+ courses with lifetime members</p>
-                        <a href="pricing-plan.html" class="btn btn-main mt-16 rounded-pill">Get Access</a>
-                    </div>
-                </div>
             </div>
         </div>
       
@@ -114,98 +104,6 @@
     </div>
 
     <div class="flex-align gap-16">
-        <div class="flex-align gap-8">
-            <!-- Notification Start -->
-            <div class="dropdown">
-                <button class="dropdown-btn shaking-animation text-gray-500 w-40 h-40 bg-main-50 hover-bg-main-100 transition-2 rounded-circle text-xl flex-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <span class="position-relative">
-                        <i class="ph ph-bell"></i>
-                        <span class="alarm-notify position-absolute end-0"></span>
-                    </span>
-                </button>
-                <div class="dropdown-menu dropdown-menu--lg border-0 bg-transparent p-0">
-                    <div class="card border border-gray-100 rounded-12 box-shadow-custom p-0 overflow-hidden">
-                        <div class="card-body p-0">
-                            <div class="py-8 px-24 bg-main-600">
-                                <div class="flex-between">
-                                    <h5 class="text-xl fw-semibold text-white mb-0">Notifications</h5>
-                                    <div class="flex-align gap-12">
-                                        <button type="button" class="bg-white rounded-6 text-sm px-8 py-2 hover-text-primary-600"> New </button>
-                                        <button type="button" class="close-dropdown hover-scale-1 text-xl text-white"><i class="ph ph-x"></i></button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-24 max-h-270 overflow-y-auto scroll-sm">
-                                <div class="d-flex align-items-start gap-12">
-                                    <img src="assets/images/thumbs/notification-img1.png" alt="" class="w-48 h-48 rounded-circle object-fit-cover">
-                                    <div class="border-bottom border-gray-100 mb-24 pb-24">
-                                        <div class="flex-align gap-4">
-                                            <a href="#" class="fw-medium text-15 mb-0 text-gray-300 hover-text-main-600 text-line-2">Ashwin Bose is requesting access to Design File - Final Project. </a>
-                                            <!-- Three Dot Dropdown Start -->
-                                            <div class="dropdown flex-shrink-0">
-                                                <button class="text-gray-200 rounded-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="ph-fill ph-dots-three-outline"></i>
-                                                </button>
-                                                <div class="dropdown-menu dropdown-menu--md border-0 bg-transparent p-0">
-                                                    <div class="card border border-gray-100 rounded-12 box-shadow-custom">
-                                                        <div class="card-body p-12">
-                                                            <div class="max-h-200 overflow-y-auto scroll-sm pe-8">
-                                                                <ul>
-                                                                    <li class="mb-0">
-                                                                        <a href="#" class="py-6 text-15 px-8 hover-bg-gray-50 text-gray-300 rounded-8 fw-normal text-xs d-block">
-                                                                            <span class="text">Mark as read</span>
-                                                                        </a>
-                                                                    </li>
-                                                                    <li class="mb-0">
-                                                                        <a href="#" class="py-6 text-15 px-8 hover-bg-gray-50 text-gray-300 rounded-8 fw-normal text-xs d-block">
-                                                                            <span class="text">Delete Notification</span>
-                                                                        </a>
-                                                                    </li>
-                                                                    <li class="mb-0">
-                                                                        <a href="#" class="py-6 text-15 px-8 hover-bg-gray-50 text-gray-300 rounded-8 fw-normal text-xs d-block">
-                                                                            <span class="text">Report</span>
-                                                                        </a>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!-- Three Dot Dropdown End -->
-                                        </div>
-                                        <div class="flex-align gap-6 mt-8">
-                                            <img src="assets/images/icons/google-drive.png" alt="">
-                                            <div class="flex-align gap-4">
-                                                <p class="text-gray-900 text-sm text-line-1">Design brief and ideas.txt</p>
-                                                <span class="text-xs text-gray-200 flex-shrink-0">2.2 MB</span>
-                                            </div>
-                                        </div>
-                                        <div class="mt-16 flex-align gap-8">
-                                            <button type="button" class="btn btn-main py-8 text-15 fw-normal px-16">Accept</button>
-                                            <button type="button" class="btn btn-outline-gray py-8 text-15 fw-normal px-16">Decline</button>
-                                        </div>
-                                        <span class="text-gray-200 text-13 mt-8">2 mins ago</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-start gap-12">
-                                    <img src="assets/images/thumbs/notification-img2.png" alt="" class="w-48 h-48 rounded-circle object-fit-cover">
-                                    <div class="">
-                                        <a href="#" class="fw-medium text-15 mb-0 text-gray-300 hover-text-main-600 text-line-2">Patrick added a comment on Design Assets - Smart Tags file:</a>
-                                        <span class="text-gray-200 text-13">2 mins ago</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="#" class="py-13 px-24 fw-bold text-center d-block text-primary-600 border-top border-gray-100 hover-text-decoration-underline"> View All </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Notification Start -->
-
-            <!-- Language Start -->
-        </div>
 
 
         <!-- User Profile Start -->
